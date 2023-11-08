94cb9c1e657b2782bea1da6c5c541e0d4daf6020
ViCode321 <110791992+ViCode321@users.noreply.github.com>
2023-11-07 18:46:06 -0600

    ..

--- orders/models.py
+++ orders/models.py
@@ -6,6 +6,7 @@ class Pizza(models.Model):
     Representa una pizza.
 
     Attributes:
+        id: Campo explícito de clave primaria.
         name: El nombre de la pizza.
         size: El tamaño de la pizza.
         price: El precio de la pizza.
@@ -13,6 +14,7 @@ class Pizza(models.Model):
         additional_ingredients: Los ingredientes adicionales de la pizza.
         image: La imagen de la pizza.
     """
+    id = models.AutoField(primary_key=True)  # Agrega un campo id explícito
     name = models.CharField(max_length=255)
     size = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -20,36 +22,37 @@ class Pizza(models.Model):
     additional_ingredients = models.ManyToManyField('AdditionalIngredient')
     image_url = models.CharField(max_length=255, default='URL_predeterminada')
 
-
-
 class Topping(models.Model):
     """
     Representa una cobertura para pizza.
 
     Attributes:
+        id: Campo explícito de clave primaria.
         name: El nombre de la cobertura.
         price: El precio de la cobertura.
     """
-
+    id = models.AutoField(primary_key=True)  # Agrega un campo id explícito
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
 
 class AdditionalIngredient(models.Model):
     """
     Representa un ingrediente adicional para pizza.
 
     Attributes:
+        id: Campo explícito de clave primaria.
         name: El nombre del ingrediente adicional.
         price: El precio del ingrediente adicional.
     """
-
+    id = models.AutoField(primary_key=True)  # Agrega un campo id explícito
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 #################################################################
 class CustomUser(AbstractUser):
     # Campos personalizados, como first_name, last_name y email
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
